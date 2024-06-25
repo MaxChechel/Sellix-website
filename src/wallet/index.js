2e34a87eef4e0224025e6320720701213dbf7d59
Max Chechel <maksymchechel@yahoo.com>
2024-06-25 23:12:06 +0200

    wallet aniamtions

--- src/wallet/index.js
+++ src/wallet/index.js
@@ -1,53 +1,115 @@
 import gsap from "gsap";
 import DrawSVGPlugin from "gsap/DrawSVGPlugin";
 gsap.registerPlugin(DrawSVGPlugin);
+
 const graphBars = document.querySelectorAll(".graphs-group path");
 
 const barHeights = [];
 const barYs = [];
 
-graphBars.forEach((bar, i) => {
-  gsap.set(bar, { drawSVG: "100% 100%", opacity: 0 });
-  gsap.to(bar, {
-    drawSVG: "0% 100%",
+const loadTl = gsap
+  .timeline()
+  .to(".wallet-hero_asset", {
+    delay: 0.2,
     opacity: 1,
-    duration: 0.6,
-    delay: 0.01 * i,
-    ease: "power4.out",
+    y: 0,
+  })
+  .add(() => {
+    graphBars.forEach((bar, i) => {
+      gsap.set(bar, { drawSVG: "100% 100%", opacity: 0 });
+      gsap.to(bar, {
+        drawSVG: "0% 100%",
+        opacity: 1,
+        duration: 0.8,
+        delay: 0.01 * i,
+        ease: "power4.out",
+      });
+    });
+  }, "<80%")
+  .add(() => {
+    spinTween();
+  }, 0)
+  .add(() => {
+    //Graph
+    new Chart(document.querySelector(".wallet-hero_ballance-graph"), {
+      type: "doughnut",
+      data: {
+        datasets: [
+          {
+            label: "Popular Cereal",
+            backgroundColor: [
+              "#6A3CE2",
+              "#E16727",
+              "#239F51",
+              "#DB2777",
+              "#2563EB",
+            ],
+            data: [48, 17, 4, 20, 11],
+            borderWidth: 0,
+            borderRadius: 40,
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        cutoutPercentage: 90,
+        borderColor: "rgba(255,255,255,0.0)",
+        legend: {
+          display: false,
+        },
+        animation: {
+          duration: 2000,
+          delay: 6000,
+        },
+        hover: {
+          mode: null,
+        },
+        tooltips: {
+          enabled: false,
+        },
+      },
+    });
   });
-  // const barHeight = parseFloat(bar.getAttribute("height"));
-  // const barY = parseFloat(bar.getAttribute("y"));
-  // barHeights.push(barHeight);
-  // barYs.push(barY);
-  // gsap.set(bar, {
-  //   attr: {
-  //     height: 0,
-  //     y: barY + barHeight,
-  //   },
-  // });
+
+/////
+const animTl = gsap.timeline({
+  defaults: { ease: "none" },
+  repeat: -1,
+  repeatDelay: 0,
+  paused: true,
 });
 
-// const heroCards = document.querySelectorAll(".fraud_hero-grid-img");
-// const cardsTl = gsap.timeline();
-// cardsTl
-//   .to(heroCards, {
-//     delay: 0.5,
-//     opacity: 1,
-//     y: 0,
-//     duration: 0.6,
-//     ease: "circ.out",
-//     stagger: { each: 0.075 },
-//   })
-//   .add(() => {
-//     graphBars.forEach((bar, i) => {
-//       gsap.to(bar, {
-//         delay: 0.025 * i,
-//         attr: {
-//           height: barHeights[i],
-//           y: barYs[i],
-//         },
-//         duration: 0.6,
-//         ease: "power4.out",
-//       });
-//     });
-//   }, "<20%");
+function spinTween() {
+  const maxTime = 2;
+  const distance = "15";
+  animTl
+
+    .to(".n-1", { y: `-=${(distance / 10) * 5}vw`, duration: maxTime / 5 })
+    .to(
+      ".n-2",
+      { y: `-=${(distance / 10) * 6}vw`, duration: maxTime / 10 },
+      "<0%"
+    )
+    .to(
+      ".n-3",
+      { y: `-=${(distance / 10) * 8}vw`, duration: maxTime / 20 },
+      "<0%"
+    )
+    .to(
+      ".n-4",
+      { y: `-=${(distance / 10) * 9}vw`, duration: maxTime / 30 },
+      "<0%"
+    )
+    .to(
+      ".n-5",
+      { y: `-=${(distance / 10) * 6}vw`, duration: maxTime / 40 },
+      "<0%"
+    )
+    .to(
+      ".n-6",
+      { y: `-=${(distance / 10) * 5}vw`, duration: maxTime / 50 },
+      "<0%"
+    );
+  gsap.to(animTl, 5, { progress: 1, ease: "power3.inOut" });
+}
